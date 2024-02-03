c793b78f4174401960cfab853e56fe453d0ce112
jiangkun0928 <jiangkun0928@focuschina.com>
2024-02-03 18:13:37 +0800

    【BIGD-6938】修改启动shell对接的clickhouse的机器IP

--- bin/start.sh
+++ bin/start.sh
@@ -4,7 +4,7 @@ PROM2CLICK_HOME=/opt/prom2click
 PROM2CLICK_LOG=/var/log/prom2click
 PROM2CLICK_PID=$PROM2CLICK_HOME/prom2click.pid
 
-nohup $PROM2CLICK_HOME/prom2click "-ch.addr" "10.110.6.181:8123" "-ch.user" "fide_w" "-ch.password" "fide@1234" "-ch.db" "fidedw" "-ch.writeTable" "ods_app_metrics_ed" "-ch.batch" "10000" > $PROM2CLICK_LOG/prom2click.log 2>&1 &
+nohup $PROM2CLICK_HOME/prom2click "-ch.addr" "10.110.6.180:8123,10.110.6.181:8123" "-ch.user" "fide_w" "-ch.password" "fide@1234" "-ch.db" "fidedw" "-ch.writeTable" "ods_app_metrics_ed" "-ch.batch" "10000" > $PROM2CLICK_LOG/prom2click.log 2>&1 &
 
 pid=$!
 sleep 2
